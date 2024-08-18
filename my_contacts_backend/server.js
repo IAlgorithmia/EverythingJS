49987831ae214f77d08cc337f2fe9ba9859b7a65
IAlgorithmia <peterudeskey@gmail.com>
2024-08-18 21:15:35 +0530

    Updates

--- my_contacts_backend/server.js
+++ my_contacts_backend/server.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const http = require("http");
-
-const dotenv = require("dotenv").config();
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+app.get("/", (req, res) => {  
+    res.send("Hello World");
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
